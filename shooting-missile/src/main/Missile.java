b2e327603da08581eceb4bfede0708a88b37b6ce
Mateus Novaes <mateussn13@hotmail.com>
2020-05-10 20:58:39 -0300

    Missile Class was created

---
+++ shooting-missile/src/main/Missile.java
@@ -0,0 +1,33 @@
+package main;
+
+public class Missile extends Sprite {
+
+	//Constants
+	private final int BOARD_WIDTH = 390;
+	private final int MISSILE_SPEED = 2;
+	
+	public Missile(int x, int y) {
+		
+		super(x, y); // Sending position attributes to superclass
+		initMissile();
+	}
+	
+	private void initMissile() {
+		
+		/* Calling superclass methods */ 
+		
+		loadImage("src/resources/missile.png");
+		getImageDimensions(); 
+	}
+	
+	public void move() {
+		
+		// Incrementing the speed
+		x += MISSILE_SPEED;
+		
+		// If Missile cross the screen
+		if(x > BOARD_WIDTH)
+			visible = false;
+		
+	}
+}
